9538dfbfae7289006a5f4f423d7b04d47dbdefb1
RekhaKadam <rekhagk2002@gmail.com>
2025-07-05 00:53:04 +0530

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import { db } from "@vercel/postgres";
 import Link from "next/link";
+import type { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
+
 
 
 const mockUrls=[
@@ -11,12 +14,15 @@ const mockImages = mockUrls.map((url, index) => ({
   id: index + 1, url,}));
 
 
-export default function HomePage() {
+export default async function HomePage() {
+const allposts = await db.query("SELECT * FROM t3gallery_image;");
+  console.log(allposts);
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4 ">{
-       [...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image)=>(
-          <div key={image.id} className="w-48">
+      <div className="flex flex-wrap gap-4 ">
+        {allposts.rows.map((post: { id: Key | null | undefined; name: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; })=> (<div key={post.id}>{post.name}</div>))},{
+       [...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image,index)=>(
+          <div key={image.id+"-"+index} className="w-48">
             
               <img src={image.url} alt="image" />
               </div>
